3b26b2b3ce1a2ddb6b21c9543a324a7bb98945c5
Chad Blake <chadblake@Chad-Blakes-MacBook-Pro.local>
2016-07-28 14:59:16 -0400

    Updated the index page to hello world

--- index.php
+++ index.php
@@ -5,11 +5,17 @@
 </head>
 <body>
 
+<div style="width:600px;margin:50px auto;border:1px solid #ccc;border-radius:10px;background:#f2f2f2;padding:20px;">
+
 <?php
 
-echo "Hello World";
+echo "<h2>Hello World</h2>";
 
 ?>
+
+<p>This is My Test Repo!</p>
+
+</div>
 
 </body>
 </html>
