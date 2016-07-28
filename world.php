249a3066514472a7456654c5bf340ea9ff8fa2a1
Chad Blake <chadblake@Chad-Blakes-MacBook-Pro.local>
2016-07-28 15:40:11 -0400

    Updates to world

--- world.php
+++ world.php
@@ -5,7 +5,8 @@ $years = 'billion';
 $galaxy = 'Milky Way';
 $galaxy_age = 13.21;
 
-echo "<p>The Earth is $age $years years old.</p>";
-echo "<p>Earth is part of the $galaxy galaxy, which is $galaxy_age years old.</p>";
+echo "<p>The Earth is <b>$age</b> $years years old.</p>";
+echo "<p>Earth is part of the <i>$galaxy</i> galaxy, which is <b>$galaxy_age</b> years old.</p>";
+echo "<p>That's as old as your mama!!</p>";
 
 ?>
